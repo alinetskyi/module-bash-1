67fd224e54a18d6f5685ef4fc276ce1a181fbfa1
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2019-08-19 01:41:11 +0300

    Ex06 done

--- ex06/ex06.sh
+++ ex06/ex06.sh
@@ -1,45 +1,10 @@
-#!/usr/bin/env bats
+#!/bin/bash
 
-source ./ex06.sh
-
-@test "echo" {
-  run map echo echo
-
-  [ "$status" -eq 0 ]
-  [ "$output" = "echo" ]
- }
-
-@test 'touch' {
-  run map touch map.txt
-
-  [ "$status" -eq 0 ]
-  [ -f "map.txt" ]
-}
-
-@test 'touch with few params' {
-  run map touch test1 test2 test3 test4 test5
-
-  [ "$status" -eq 0 ]
-  [ -f "map.txt" ]
-}
-
-@test 'cat' {
-  run map cat map.txt
-
-  [ "$status" -eq 0 ]
-  [ "$output" = "" ]
-}
-
- @test 'cat with few params' {
-  run map cat test1 test2 test3 test4 test5
-
-  [ "$status" -eq 0 ]
-  [ "$output" = "" ]
- }
-
-@test 'rm' {
-  run map rm map.txt
-
-  [ "$status" -eq 0 ]
-  ! [ map.txt -f  ]
+map() {
+  cmd=$1
+  for i in $#
+  do
+   shift
+   eval $cmd "$1"
+  done
 }
