303da9f26cf9b32f6c5e232e930dc754847ed097
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2019-08-19 02:44:28 +0300

    Ex07 done

--- ex07/ex07.sh
+++ ex07/ex07.sh
@@ -1,1 +1,14 @@
-#!/usr/bin/env bats
+#!/bin/bash
+
+if [[ $# != 1 ]] || [[ $1 == "0" ]]
+then
+    exit 1
+fi
+
+data=$(head -n $1 resourses/surnames.txt)
+data=$(echo "$data" | grep -v "Q-Chem")
+data=$(echo "$data" | sed 's/\.//;s/- / /')
+
+
+echo "$data"
+exit 0
