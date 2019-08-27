636f0ef70cf9d0edec278b9b6e20ec2ef44798eb
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2019-08-27 03:50:09 +0300

    Ex08 done

--- ex08/ex08.sh
+++ ex08/ex08.sh
@@ -1,1 +1,25 @@
-#!/usr/bin/env bats
+#!bin/bash
+
+if [ $# -eq 0 ] || [ $# -gt 1 ];then
+	echo "Too many(few) arguments"
+	exit 1
+fi
+
+filename=$1
+
+if ! [ -f "$filename" ];then
+	echo "Error.."
+	exit 1
+fi
+
+numbers=('zeroes' 'ones' 'twoes' 'threes' 'fours' 'fives' 'sixs' 'sevens' 'eights' 'nines')
+
+for ((i=0; i<9; i++));
+do
+	printf "$(grep -o "$i" $filename | wc -l)"
+	printf " ${numbers[$i]}, "
+done
+printf "$(grep -o "$i" $filename | wc -l)"
+printf " ${numbers[$i]}"
+printf "\n"
+
